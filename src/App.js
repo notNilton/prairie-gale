2d3d7fe44095b8343591659a68cf8d6dbbd32dd4
notNilton <nilton.naab@gmail.com>
2024-07-17 14:04:25 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -25,29 +25,6 @@ function App() {
     }
   };
 
-  // const addMember = async () => {
-  //   if (!newMember.trim()) return;
-
-  //   try {
-  //     const response = await fetch("/members", {
-  //       method: "POST",
-  //       headers: {
-  //         "Content-Type": "application/json",
-  //       },
-  //       body: JSON.stringify({ name: newMember }),
-  //     });
-
-  //     if (response.ok) {
-  //       setNewMember("");
-  //       fetchMembers();
-  //     } else {
-  //       console.error("Failed to add member");
-  //     }
-  //   } catch (error) {
-  //     console.error("Error adding member:", error);
-  //   }
-  // };
-
   const reconcileData = async () => {
     try {
       const response = await fetch("/reconcile", {
@@ -79,15 +56,6 @@ function App() {
 
   return (
     <div>
-      <h1>Input</h1>
-      {/* <input
-        type="text"
-        value={newMember}
-        onChange={(e) => setNewMember(e.target.value)}
-        placeholder="New Member Name"
-      />
-      <button onClick={addMember}>Add Member</button> */}
-
       <h2>Reconciliation</h2>
       <textarea
         value={incidenceMatrix}
